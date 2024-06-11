74479ae0272e11e76b87719b5d12056439096c67
HengLeap Ear <102787289+EarHengLeap@users.noreply.github.com>
2024-06-11 15:44:36 +0700

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import Image from "next/image";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { FaHome } from "react-icons/fa";
 
-export default function Home() {
+const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav
+          navItems={[
+            {
+              name: "Home",
+              link: "/",
+              icon: <FaHome />,
+            },
+          ]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
-}
+};
+
+export default Home;
